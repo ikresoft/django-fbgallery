495ea1108099366948cbc4b955011277d71db144
Mirza Delic <mirzadelic1@gmail.com>
2014-10-09 13:51:15 +0200

    fix

--- fbgallery/urls.py
+++ fbgallery/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import *
-from django.conf import settings
+# from django.conf import settings
 
-fb_id = getattr(settings, 'FB_PAGE_ID', None)
+# fb_id = getattr(settings, 'FB_PAGE_ID', None)
 
 urlpatterns = patterns('fbgallery.views',
-    url(r'^$', 'display_albums', {'fb_id': fb_id,}, name='fb-albums'),
-    url(r'^(?P<album_id>[-\w]+)/$', 'display_album', {'fb_id': fb_id,}, name='fb-album'),
+    url(r'^$', 'display_albums', name='fb-albums'),
+    url(r'^(?P<album_id>[-\w]+)/$', 'display_album', name='fb-album'),
 )
