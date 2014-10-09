adb90156ad04a94f431bc519fdd5411e82ad1df3
Mirza Delic <mirzadelic1@gmail.com>
2014-10-09 13:08:58 +0200

    fix

--- fbgallery/views.py
+++ fbgallery/views.py
@@ -7,7 +7,8 @@ from django.core.cache import cache
 from django.conf import settings
 
 graph_url = 'https://graph.facebook.com'
-albums_url = '%s/%s/albums?fields=id,name,cover_photo&limit=10000&access_token=%s|%s' % (graph_url, settings.FB_PAGE_ID, settings.FB_APP_ID, settings.FB_APP_SECRET)
+access_token = 'access_token=%s|%s' % (settings.FB_APP_ID, settings.FB_APP_SECRET)
+albums_url = '%s/%s/albums?fields=id,name,cover_photo&limit=10000&%s' % (graph_url, settings.FB_PAGE_ID, access_token)
 cache_expires = getattr(settings, 'CACHE_EXPIRES', 30)
 
 
@@ -26,7 +27,7 @@ def get_graph_result(url):
     return data
 
 
-def display_albums(request, fb_id):
+def display_albums(request):
     albums = get_graph_result(albums_url)["data"]
     for i in range(len(albums)):
         album = albums[i]
@@ -39,21 +40,9 @@ def display_albums(request, fb_id):
     return render_to_response('fbgallery/albums.html', context_instance=data)
 
 
-def display_album(request, album_id, fb_id):
-    album = get_graph_result("%s/%s" % (graph_url, album_id))
-    photos_url = "%s/%s/photos" % (graph_url, album_id)
-    photos = get_graph_result(photos_url)["data"]
-
-    data = RequestContext(request, {
-        'album': album,
-        'photos': photos,
-    })
-    return render_to_response('fbgallery/album.html', context_instance=data)
-
-
 def return_album(album_id, limit=16):
     album = get_graph_result("%s/%s" % (graph_url, album_id))
-    photos_url = "%s/%s/photos" % (graph_url, album_id)
+    photos_url = "%s/%s/photos?%s" % (graph_url, album_id, access_token)
     photos = get_graph_result(photos_url)["data"][:limit]
 
     data = {
@@ -61,3 +50,8 @@ def return_album(album_id, limit=16):
         'photos': photos,
     }
     return data
+
+
+def display_album(request, album_id):
+    data = RequestContext(request, return_album(album_id, 1000))
+    return render_to_response('fbgallery/album.html', context_instance=data)
